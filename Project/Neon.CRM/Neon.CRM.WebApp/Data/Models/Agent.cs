831e473dd8c343a35da36111cecc1b14a2c047e4
LeeTrent <lee.trent.1@gmail.com>
2025-05-15 21:06:49 -0500

    created data model and ran migration

---
+++ Project/Neon.CRM/Neon.CRM.WebApp/Data/Models/Agent.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace Neon.CRM.WebApp.Data.Models
+{
+    public class Agent : IdentityUser
+    {
+        public required string FirstName { get; set; }
+        public required string LastName { get; set; }
+
+        public ICollection<Customer> Customers { get; set; } = [];
+
+    }
+}
---
+++ Project/Neon.CRM/Neon.CRM.WebApp/Data/Models/VacationPackage.cs
@@ -0,0 +1,14 @@
+﻿namespace Neon.CRM.WebApp.Data.Models;
+
+public class VacationPackage
+{
+    public int Id { get; set; }
+
+    public required string Title { get; set; }
+    public required string Description { get; set; }
+    public decimal Price { get; set; }
+    public int DurationInDays { get; set; }
+
+    // Navigation property
+    public ICollection<Booking> Bookings { get; set; } = [];
+}
