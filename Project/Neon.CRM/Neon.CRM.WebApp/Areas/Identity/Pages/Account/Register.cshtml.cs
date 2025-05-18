f661bff0e3f13723c80e4d6363f942b9bb54f17c
LeeTrent <lee.trent.1@gmail.com>
2025-05-17 22:36:25 -0500

    Completed user registration and login functionality

--- Project/Neon.CRM/Neon.CRM.WebApp/Areas/Identity/Pages/Account/Register.cshtml.cs
+++ Project/Neon.CRM/Neon.CRM.WebApp/Areas/Identity/Pages/Account/Register.cshtml.cs
@@ -71,6 +71,14 @@ namespace Neon.CRM.WebApp.Areas.Identity.Pages.Account
         /// </summary>
         public class InputModel
         {
+            [Required]
+            [StringLength(100, ErrorMessage = "The {0} must be at least {2} and at max {1} characters long.", MinimumLength = 2)]
+            public string FirstName { get; set; }
+
+            [Required]
+            [StringLength(100, ErrorMessage = "The {0} must be at least {2} and at max {1} characters long.", MinimumLength = 2)]
+            public string LastName { get; set; }
+
             /// <summary>
             ///     This API supports the ASP.NET Core Identity default UI infrastructure and is not intended to be used
             ///     directly from your code. This API may change or be removed in future releases.
@@ -117,6 +125,10 @@ namespace Neon.CRM.WebApp.Areas.Identity.Pages.Account
 
                 await _userStore.SetUserNameAsync(user, Input.Email, CancellationToken.None);
                 await _emailStore.SetEmailAsync(user, Input.Email, CancellationToken.None);
+
+                user.FirstName = Input.FirstName;
+                user.LastName = Input.LastName;
+
                 var result = await _userManager.CreateAsync(user, Input.Password);
 
                 if (result.Succeeded)
